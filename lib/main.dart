a45ab08845d53ff3e86346fffffc3e26c5403199
aditmail <aditmail321@gmail.com>
2023-01-19 20:51:11 +0700

    setup onGenerateRoute and onUnknownRoute

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,10 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
           canvasColor: Color.fromRGBO(255, 254, 229, 1),
           fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
+          textTheme: ThemeData
+              .light()
+              .textTheme
+              .copyWith(
               bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               headline6: TextStyle(
@@ -28,6 +31,13 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealScreen.routeName: (ctx) => CategoryMealScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+      onUnknownRoute: (settings){
+       return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
